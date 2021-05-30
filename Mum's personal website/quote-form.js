1d5376641958918720b595aec1302e3efee0b2de
Emmanuel Eyo <76617609+EmmanuelEyo@users.noreply.github.com>
2021-05-30 18:23:08 +0100

    Add files via upload

---
+++ Mum's personal website/quote-form.js
@@ -0,0 +1,7 @@
+$("#submitForm").click(function () {
+  swal(
+    "Congratulations!",
+    "You have successfully submitted the form! Check your email for further information.",
+    "success"
+  );
+});
